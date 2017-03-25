020bba142860860dfd12bf622ed9bce83ed33e1c
Krzysztof Barański <Krzysztof Baraski>
2017-03-25 16:53:25 +0100

    pole trojkata

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+// plik scripts.js
+
+var a = 6;
+var h = 2;
+
+a = prompt('Wpisz d³ugoœæ a');
+h = prompt('Wpisz d³ugoœæ h');
+
+
+var triangleArea = (a*h/2);
+console.log('Pole trójk¹ta o podstawie a: ' + a + ' i wysokoœci h: ' + h + ' wynosi: ' + triangleArea);
+alert('Pole trójk¹ta o podstawie a: ' + a + ' i wysokoœci h: ' + h + ' wynosi: ' + triangleArea);
